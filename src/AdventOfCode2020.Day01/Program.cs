68624431a3f754d1376e470622527c1c17a4828a
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-15 12:50:10 +0100

    feat: complete day 1, puzzle 2

--- src/AdventOfCode2020.Day01/Program.cs
+++ src/AdventOfCode2020.Day01/Program.cs
@@ -8,17 +8,27 @@ var numbers = lines
     .Select(int.Parse)
     .ToArray();
 
+var (solution1, solution2) = (0, 0);
+
 for (var i = 0; i < numbers.Length; i++)
 {
     for (var j = i + 1; j < numbers.Length; j++)
     {
         if (numbers[i] + numbers[j] == 2020)
         {
-            var solution = numbers[i] * numbers[j];
+            solution1 = numbers[i] * numbers[j];
+        }
 
-            Console.WriteLine($"Day 01 - Puzzle 1: {solution}");
-
-            return;
+        for (var k = j + 1; k < numbers.Length; k++)
+        {
+            if (numbers[i] + numbers[j] + numbers[k] == 2020)
+            {
+                solution2 = numbers[i] * numbers[j] * numbers[k];
+            }
         }
     }
 }
+
+Console.WriteLine($"Day 1 - Puzzle 1: {solution1}");
+
+Console.WriteLine($"Day 1 - Puzzle 2: {solution2}");
