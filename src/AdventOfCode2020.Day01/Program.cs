9af22d5e46da7930b3c8e372565b7be44fa80d1a
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-15 12:45:05 +0100

    feat: complete day 1, puzzle 1

---
+++ src/AdventOfCode2020.Day01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var numbers = lines
+    .Select(int.Parse)
+    .ToArray();
+
+for (var i = 0; i < numbers.Length; i++)
+{
+    for (var j = i + 1; j < numbers.Length; j++)
+    {
+        if (numbers[i] + numbers[j] == 2020)
+        {
+            var solution = numbers[i] * numbers[j];
+
+            Console.WriteLine($"Day 01 - Puzzle 1: {solution}");
+
+            return;
+        }
+    }
+}
