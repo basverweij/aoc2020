976ab75b76a46b5c41c3c14922d27a0808682362
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-15 15:01:58 +0100

    feat: complete day 3, puzzle 1

---
+++ src/AdventOfCode2020.Day03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+var geo = await File.ReadAllLinesAsync("input.txt");
+
+var (x, trees) = (0, 0);
+
+for (var y = 1; y < geo.Length; y++)
+{
+    x = ( x + 3 ) % geo[y].Length;
+
+    if (geo[y][x] == '#')
+    {
+        trees++;
+    }
+}
+
+Console.WriteLine($"Day 3 - Puzzle 1: {trees}");
