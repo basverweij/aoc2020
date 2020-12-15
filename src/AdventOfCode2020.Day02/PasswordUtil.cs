b08b2e9bddef4ee1341da73825d7adff77c857fc
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-15 13:19:46 +0100

    feat: complete day 2. puzzle 1

---
+++ src/AdventOfCode2020.Day02/PasswordUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020.Day02
+{
+    public static class PasswordUtil
+    {
+        private static readonly Regex _passwordPattern = new Regex(
+            "^(?<min>[0-9]+)-(?<max>[0-9]+) (?<letter>[a-z]): (?<password>[a-z]+)$",
+            RegexOptions.Compiled);
+
+        public static bool IsValid(
+            string line)
+        {
+            var match = _passwordPattern.Match(line);
+
+            if (!match.Success)
+            {
+                throw new InvalidOperationException($"invalid password: {line}");
+            }
+
+            var min = int.Parse(match.Groups[1].Value);
+
+            var max = int.Parse(match.Groups[2].Value);
+
+            var letter = match.Groups[3].Value[0];
+
+            var password = match.Groups[4].Value;
+
+            var count = password.Count(c => c == letter);
+
+            return count >= min && count <= max;
+        }
+    }
+}
---
+++ src/AdventOfCode2020.Day02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+using AdventOfCode2020.Day02;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var solution1 = lines.Count(PasswordUtil.IsValid);
+
+Console.WriteLine($"Day 2 - Puzzle 1: {solution1}");
+
