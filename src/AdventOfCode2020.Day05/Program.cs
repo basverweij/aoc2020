16e00ee559e728d40f87938fe70341cb6bb511d5
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-20 12:14:21 +0100

    feat: complete day 5, puzzle 2

--- src/AdventOfCode2020.Day05/Program.cs
+++ src/AdventOfCode2020.Day05/Program.cs
@@ -12,8 +12,23 @@ var seatIds = lines
                 .Replace('L', '0')
                 .Replace('R', '1'),
             2))
+    .OrderBy(i => i)
     .ToArray();
 
 var solution1 = seatIds.Max();
 
 Console.WriteLine($"Day 5 - Puzzle 1: {solution1}");
+
+var solution2 = 0;
+
+for (var i = 0; i < seatIds.Length - 1; i++)
+{
+    if (seatIds[i + 1] - seatIds[i] == 2)
+    {
+        solution2 = seatIds[i] + 1;
+
+        break;
+    }
+}
+
+Console.WriteLine($"Day 5 - Puzzle 2: {solution2}");
