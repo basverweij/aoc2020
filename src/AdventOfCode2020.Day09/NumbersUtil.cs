c047d1a83e2f2573d3eb02da962bdda8dc6e79dc
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-20 17:35:24 +0100

    feat: complete day 9, puzzle 1

---
+++ src/AdventOfCode2020.Day09/NumbersUtil.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2020.Day09
+{
+    public static class NumbersUtil
+    {
+        public static bool IsValid(
+            this ISet<long> @this,
+            long number)
+        {
+            foreach (var i in @this)
+            {
+                foreach (var j in @this)
+                {
+                    if (i == j)
+                    {
+                        // numbers must be different
+
+                        continue;
+                    }
+
+                    if (i + j == number)
+                    {
+                        // valid number
+
+                        return true;
+                    }
+
+                    if (i + j > number)
+                    {
+                        // no more valid combinations w/ current i
+
+                        break;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/AdventOfCode2020.Day09/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using AdventOfCode2020.Day09;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var preamble = lines.Take(25).Select(long.Parse).ToArray();
+
+var numbers = lines.Skip(25).Select(long.Parse).ToArray();
+
+var window = new Queue<long>(preamble);
+
+var sorted = new SortedSet<long>(preamble);
+
+var solution1 = 0L;
+
+foreach (var number in numbers)
+{
+    // check whether number is valid
+
+    if (!sorted.IsValid(number))
+    {
+        solution1 = number;
+
+        break;
+    }
+
+    // advance window
+
+    sorted.Remove(window.Dequeue());
+
+    window.Enqueue(number);
+
+    sorted.Add(number);
+}
+
+Console.WriteLine($"Day 9 - Puzzle 1: {solution1}");
