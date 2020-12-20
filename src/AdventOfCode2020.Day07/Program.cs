10f566b8dc93c1faad4d84c1ad3ca17b44957b43
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-20 14:08:58 +0100

    feat: complete day 7, puzzle 1

---
+++ src/AdventOfCode2020.Day07/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using AdventOfCode2020.Day07;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var rules = lines.Select(RulesUtil.ParseRule).ToArray();
+
+var containedBy = new Dictionary<string, HashSet<string>>();
+
+foreach (var rule in rules)
+{
+    foreach (var (color, _) in rule.contains)
+    {
+        if (!containedBy.ContainsKey(color))
+        {
+            containedBy[color] = new();
+        }
+
+        containedBy[color].Add(rule.color);
+    }
+}
+
+var canContainShinyGold = new HashSet<string>();
+
+var todo = new Queue<string>();
+
+todo.Enqueue("shiny gold");
+
+while (todo.Any())
+{
+    var color = todo.Dequeue();
+
+    if (!containedBy.TryGetValue(color, out var containingColors))
+    {
+        continue;
+    }
+
+    foreach (var containingColor in containingColors)
+    {
+        if (canContainShinyGold.Contains(containingColor))
+        {
+            continue;
+        }
+
+        canContainShinyGold.Add(containingColor);
+
+        todo.Enqueue(containingColor);
+    }
+}
+
+var solution1 = canContainShinyGold.Count;
+
+Console.WriteLine($"Day 7 - Puzzle 1: {solution1}");
---
+++ src/AdventOfCode2020.Day07/RulesUtil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2020.Day07
+{
+    public static class RulesUtil
+    {
+        public static (string color, (string color, int count)[] contains) ParseRule(
+            string line)
+        {
+            if (line[^1] != '.')
+            {
+                throw new ArgumentException($"invalid line '{line}'", nameof(line));
+            }
+
+            var parts = line[..^1].Split(" bags contain ");
+
+            if (parts.Length != 2)
+            {
+                throw new ArgumentException($"invalid line '{line}'", nameof(line));
+            }
+
+            if (parts[1] == "no other bags")
+            {
+                return (parts[0], new (string color, int count)[0]);
+            }
+
+            var contains = parts[1]
+                .Split(", ")
+                .Select(c =>
+                {
+                    var containsParts = c.Split(' ');
+
+                    if (!int.TryParse(containsParts[0], out var count))
+                    {
+                        throw new ArgumentException($"invalid contains part '{c}'", nameof(c));
+                    }
+
+                    return ($"{containsParts[1]} {containsParts[2]}", count);
+                })
+                .ToArray();
+
+            return (parts[0], contains);
+        }
+    }
+}
