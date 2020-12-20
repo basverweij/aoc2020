ee4617dfbc88a95c5b771e50de45319a25d2b3da
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-20 15:20:41 +0100

    feat: complete day 8, puzzle 2

--- src/AdventOfCode2020.Day08/Program.cs
+++ src/AdventOfCode2020.Day08/Program.cs
@@ -24,3 +24,46 @@ while (!visitedPcs.Contains(pc))
 
 Console.WriteLine($"Day 8 - Puzzle 1: {accumulator}");
 
+for (var i = 0; i < instructions.Length; i++)
+{
+    if (instructions[i].opcode == Opcodes.Acc)
+    {
+        // only change Jmp and Nop opcodes
+
+        continue;
+    }
+
+    var variant = instructions.ToArray(); // create a copy
+
+    // flip opcode
+
+    variant[i].opcode =
+        variant[i].opcode == Opcodes.Jmp ?
+            Opcodes.Nop :
+            Opcodes.Jmp;
+
+    // run variant
+
+    accumulator = 0;
+
+    pc = 0;
+
+    visitedPcs.Clear();
+
+    while (!visitedPcs.Contains(pc) &&
+        pc != variant.Length)
+    {
+        visitedPcs.Add(pc);
+
+        variant[pc].Execute(ref pc, ref accumulator);
+    }
+
+    if (pc == variant.Length)
+    {
+        // completed successfulyy
+
+        break;
+    }
+}
+
+Console.WriteLine($"Day 8 - Puzzle 2: {accumulator}");
