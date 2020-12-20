9adcf48435ae5fa8a87f3d18730419acf8545fc9
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-20 12:10:09 +0100

    feat: complete day 5, puzzle 1

---
+++ src/AdventOfCode2020.Day05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var seatIds = lines
+    .Select(
+        s => Convert.ToInt32(
+            s.Replace('F', '0')
+                .Replace('B', '1')
+                .Replace('L', '0')
+                .Replace('R', '1'),
+            2))
+    .ToArray();
+
+var solution1 = seatIds.Max();
+
+Console.WriteLine($"Day 5 - Puzzle 1: {solution1}");
