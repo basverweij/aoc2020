5652ec5cf368ff516d37ba2e6eaa25d0538d9dea
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-20 12:24:58 +0100

    feat: complete day 6, puzzle 1

---
+++ src/AdventOfCode2020.Day06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var counts = new List<int>();
+
+var answers = new HashSet<char>();
+
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        counts.Add(answers.Count);
+
+        answers.Clear();
+
+        continue;
+    }
+
+    foreach (var answer in line)
+    {
+        answers.Add(answer);
+    }
+}
+
+counts.Add(answers.Count);
+
+var solution1 = counts.Sum();
+
+Console.WriteLine($"Day 6 - Puzzle 1: {solution1}");
