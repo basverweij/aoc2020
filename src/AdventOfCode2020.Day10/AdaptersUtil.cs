719cbe3787cbe38798c2c1dab9f1bcfc8c4a32a9
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-23 18:39:30 +0100

    feat: complete day 10, puzzle 2

---
+++ src/AdventOfCode2020.Day10/AdaptersUtil.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day10
+{
+    public static class AdaptersUtil
+    {
+        public static long GetArrangements(
+            this int[] @this)
+        {
+            var n = 0;
+
+            var todo = new Queue<int>();
+
+            todo.Enqueue(0);
+
+            while (todo.Any())
+            {
+                var i = todo.Dequeue();
+
+                for (var j = i + 1; j < i + 4; j++)
+                {
+                    if (j == @this.Length - 1)
+                    {
+                        n++;
+
+                        break;
+                    }
+
+                    if (@this[j] - @this[i] > 3)
+                    {
+                        break;
+                    }
+
+                    todo.Enqueue(j);
+                }
+            }
+
+            return n;
+        }
+    }
+}
--- src/AdventOfCode2020.Day10/Program.cs
+++ src/AdventOfCode2020.Day10/Program.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+using AdventOfCode2020.Day10;
+
 var lines = await File.ReadAllLinesAsync("input.txt");
 
-var adapters = lines
+var values = lines
     .Select(int.Parse)
     .OrderBy(a => a)
-    .ToArray();
+    .ToList();
 
-var differences = new Dictionary<int, int>
-{
-    [3] = 1, // built-in adapter is always 3 jolts higher than the highest adapter
-};
+values.Insert(0, 0); // charging outlet, used for solution 2
+
+values.Add(values.Max() + 3); // built-in adapter is always 3 jolts higher than the highest adapter
+
+var adapters = values.ToArray();
+
+var differences = new Dictionary<int, int>();
 
 var jolts = 0;
 
-foreach (var adapter in adapters)
+foreach (var adapter in adapters.Skip(1))
 {
     if (jolts < adapter - 3)
     {
@@ -39,3 +44,28 @@ foreach (var adapter in adapters)
 var solution1 = differences[1] * differences[3];
 
 Console.WriteLine($"Day 10 - Puzzle 1: {solution1}");
+
+var arrangements = 1L;
+
+for (var i = 0; i < adapters.Length - 1;)
+{
+    // find next anchor (i.e. two adapters that have a 3 jolt rating difference)
+
+    var j = i + 1;
+
+    while (adapters[j] - adapters[j - 1] != 3)
+    {
+        j++;
+    }
+
+    if (j - i > 2)
+    {
+        // get arrangements between previous and next anchors
+
+        arrangements *= adapters[i..j].GetArrangements();
+    }
+
+    i = j;
+}
+
+Console.WriteLine($"Day 10 - Puzzle 2: {arrangements}");
