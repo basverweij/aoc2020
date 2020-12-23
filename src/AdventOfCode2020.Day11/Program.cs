6a2a0ce17c7203bdb25e406d4ef9106a7c01107e
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-23 19:20:04 +0100

    feat: complete day 11, puzzle 1

---
+++ src/AdventOfCode2020.Day11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+using AdventOfCode2020.Day11;
+
+var lines = ( await File
+    .ReadAllLinesAsync("input.txt") )
+    .Select(s => $".{s}.") // add floor tiles left and right to remove need for boundary checks
+    .ToList();
+
+// add floor tiles above and below to remove need for boundary checks
+
+lines.Insert(0, new string('.', lines[0].Length));
+
+lines.Add(new string('.', lines[0].Length));
+
+var current = lines.Select(s => s.ToCharArray()).ToArray();
+
+var next = lines.Select(s => s.ToCharArray()).ToArray();
+
+while (current.ApplyRules(next) > 0)
+{
+    (current, next) = (next, current);
+}
+
+var solution1 = current.GetOccupied();
+
+Console.WriteLine($"Day 11 - Puzzle 1: {solution1}");
---
+++ src/AdventOfCode2020.Day11/SeatingUtil.cs
@@ -0,0 +1,75 @@
+﻿namespace AdventOfCode2020.Day11
+{
+    public static class SeatingUtil
+    {
+        public static int ApplyRules(
+            this char[][] @this,
+            char[][] next)
+        {
+            var n = 0;
+
+            for (var i = 1; i < @this.Length - 1; i++)
+            {
+                for (var j = 1; j < @this[i].Length - 1; j++)
+                {
+                    next[i][j] = @this[i][j] switch
+                    {
+                        'L' => @this.GetOccupied(i, j) switch
+                        {
+                            0 => '#',
+                            _ => 'L',
+                        },
+                        '#' => @this.GetOccupied(i, j) switch
+                        {
+                            >= 4 => 'L',
+                            _ => '#',
+                        },
+                        char c => c,
+                    };
+
+                    if (@this[i][j] != next[i][j])
+                    {
+                        n++;
+                    }
+                }
+            }
+
+            return n;
+        }
+
+        public static int GetOccupied(
+            this char[][] @this)
+        {
+            var n = 0;
+
+            for (var i = 1; i < @this.Length - 1; i++)
+            {
+                for (var j = 1; j < @this[i].Length; j++)
+                {
+                    if (@this[i][j] == '#')
+                    {
+                        n++;
+                    }
+                }
+            }
+
+            return n;
+        }
+
+        private static int GetOccupied(
+            this char[][] @this,
+            int i,
+            int j)
+        {
+            return
+                ( @this[i - 1][j - 1] == '#' ? 1 : 0 ) +
+                ( @this[i - 1][j] == '#' ? 1 : 0 ) +
+                ( @this[i - 1][j + 1] == '#' ? 1 : 0 ) +
+                ( @this[i][j - 1] == '#' ? 1 : 0 ) +
+                ( @this[i][j + 1] == '#' ? 1 : 0 ) +
+                ( @this[i + 1][j - 1] == '#' ? 1 : 0 ) +
+                ( @this[i + 1][j] == '#' ? 1 : 0 ) +
+                ( @this[i + 1][j + 1] == '#' ? 1 : 0 );
+        }
+    }
+}
