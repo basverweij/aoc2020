6c101fa572df40840a2ba3b10e5c40472321e34a
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-24 15:00:02 +0100

    feat: complete day 13, puzzle 1

---
+++ src/AdventOfCode2020.Day13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var timestamp = int.Parse(lines[0]);
+
+var ids = lines[1]
+    .Split(',')
+    .Where(s => s != "x")
+    .Select(int.Parse)
+    .ToArray();
+
+var solution1 = 0;
+
+for (var t = timestamp; solution1 == 0; t++)
+{
+    foreach (var id in ids)
+    {
+        if (t % id == 0)
+        {
+            solution1 = ( t - timestamp ) * id;
+        }
+    }
+}
+
+Console.WriteLine($"Day 13 - Puzzle 1: {solution1}");
