9e20c500e84efdb8b24eb031e26cc9b90826954a
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-27 12:38:24 +0100

    feat: complete day 15, puzzle 2

--- src/AdventOfCode2020.Day15/Program.cs
+++ src/AdventOfCode2020.Day15/Program.cs
@@ -40,3 +40,27 @@ while (++turn <= 2020)
 var solution1 = numbers.Single(kvp => kvp.Value.LastTurn == turn - 1).Key;
 
 Console.WriteLine($"Day 15 - Puzzle 1: {solution1}");
+
+--turn;
+
+while (++turn <= 30_000_000)
+{
+    if (numbers.TryGetValue(number, out var stats))
+    {
+        number = turn - stats.LastTurn;
+
+        stats.Count++;
+
+        stats.LastTurn = turn;
+    }
+    else
+    {
+        numbers[number] = new(1, turn);
+
+        number = 0;
+    }
+}
+
+var solution2 = numbers.Single(kvp => kvp.Value.LastTurn == turn - 1).Key;
+
+Console.WriteLine($"Day 15 - Puzzle 2: {solution2}");
