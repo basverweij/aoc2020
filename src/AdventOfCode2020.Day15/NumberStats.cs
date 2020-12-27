34ef9f20db13b2b0b9d4842883503e149c9fa114
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-27 12:33:17 +0100

    feat: complete day 15, puzzle 1

---
+++ src/AdventOfCode2020.Day15/NumberStats.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2020.Day15
+{
+    public class NumberStats
+    {
+        public int Count { get; set; }
+
+        public int LastTurn { get; set; }
+
+        public NumberStats(
+            int count,
+            int lastTurn)
+        {
+            Count = count;
+
+            LastTurn = lastTurn;
+        }
+
+        public override string? ToString() => $"{{Count={Count}, LastTurn={LastTurn}}}";
+    }
+}
---
+++ src/AdventOfCode2020.Day15/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AdventOfCode2020.Day15;
+
+var input = "5,2,8,16,18,0,1";
+
+var start = input.Split(',').Select(int.Parse).ToArray();
+
+var numbers = new Dictionary<int, NumberStats>();
+
+var turn = 0;
+
+foreach (var n in start)
+{
+    numbers[n] = new(1, ++turn);
+}
+
+var number = 0;
+
+while (++turn <= 2020)
+{
+    if (numbers.TryGetValue(number, out var stats))
+    {
+        number = turn - stats.LastTurn;
+
+        stats.Count++;
+
+        stats.LastTurn = turn;
+    }
+    else
+    {
+        numbers[number] = new(1, turn);
+
+        number = 0;
+    }
+}
+
+var solution1 = numbers.Single(kvp => kvp.Value.LastTurn == turn - 1).Key;
+
+Console.WriteLine($"Day 15 - Puzzle 1: {solution1}");
