9a17bc976468a281dd29850a8b352f5665ac1184
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-29 10:05:39 +0100

    feat: complete day 20, puzzle 1

---
+++ src/AdventOfCode2020.Day20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using AdventOfCode2020.Day20;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var tiles = new Dictionary<int, string[]>();
+
+for (var i = 0; i < lines.Length; i += 12)
+{
+    var id = int.Parse(lines[i][5..^1]);
+
+    var tile = lines[( i + 1 )..( i + 11 )];
+
+    tiles[id] = tile;
+}
+
+var options = tiles.ToDictionary(
+    kvp => kvp.Key,
+    kvp => kvp.Value.BuildOptions());
+
+var borderCounts = options.Values.SelectMany(o => o.SelectMany(o => o.Borders).Distinct()).GroupBy(b => b).ToDictionary(g => g.Key, g => g.Count());
+
+// border tiles have at least one border that has a unique value
+
+var borderTiles = borderCounts.Where(kvp => kvp.Value == 1).Select(kvp => kvp.Key).ToHashSet();
+
+// corner tiles have exactly 2 borders with a unique value
+// we're searching for 4 as there are two solutions (the complete puzzle can be flipped)
+
+var cornerTiles = options.Where(kvp => kvp.Value.SelectMany(o => o.Borders).Distinct().Count(borderTiles.Contains) == 4).Select(kvp => kvp.Key).ToArray();
+
+var solution1 = cornerTiles.Aggregate(1L, (a, b) => a * b);
+
+Console.WriteLine($"Day 20 - Puzzle 1: {solution1}");
---
+++ src/AdventOfCode2020.Day20/TilesUtil.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AdventOfCode2020.Day20
+{
+    [DebuggerDisplay("Top = {Borders[0]}, Right = {Borders[1]}, Bottom = {Borders[2]}, Left = {Borders[3]}")]
+    public record TileOption(int[] Borders);
+
+    public static class TilesUtil
+    {
+        public static TileOption[] BuildOptions(
+            this string[] @this)
+        {
+            // rotated 0, 90, 180, 270 degrees x original, flipped = 8 options
+
+            var options = new TileOption[8];
+
+            for (var f = 0; f < 2; f++) // flip
+            {
+                var borders = new int[4];
+
+                for (var i = 0; i < 4; i++)
+                {
+                    borders[i] = @this.GetBorder(i, f == 1);
+                }
+
+                for (var i = 0; i < 4; i++) // rotate
+                {
+                    var optionBorders = new int[4];
+
+                    for (var j = 0; j < 4; j++)
+                    {
+                        optionBorders[j] = borders[( i + j ) % 4];
+                    }
+
+                    options[4 * f + i] = new(optionBorders);
+                }
+            }
+
+            return options;
+        }
+
+        private static int GetBorder(
+            this string[] @this,
+            int index,
+            bool flipped)
+        {
+            var border = index switch
+            {
+                // top
+                0 => @this[0],
+
+                // right
+                1 => new string(Enumerable.Range(0, @this.Length).Select(i => @this[i][^1]).ToArray()),
+
+                // bottom
+                2 => @this[^1],
+
+                // left
+                3 => new string(Enumerable.Range(0, @this.Length).Select(i => @this[i][0]).ToArray()),
+
+                _ => throw new ArgumentException($"invalid index: '{index}'", nameof(index)),
+            };
+
+            if (flipped)
+            {
+                var c = border.ToCharArray();
+
+                Array.Reverse(c);
+
+                border = new string(c);
+            }
+
+            return Convert.ToInt32(new string(border.Select(c => c == '#' ? '1' : '0').ToArray()), 2);
+        }
+    }
+}
