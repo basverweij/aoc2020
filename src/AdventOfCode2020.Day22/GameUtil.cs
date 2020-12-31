281160b52c2ec691078704aaad0fd5c6854a5882
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-31 10:00:51 +0100

    feat: complete day 22, puzzle 1

---
+++ src/AdventOfCode2020.Day22/GameUtil.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day22
+{
+    public static class GameUtil
+    {
+        public static void Play(
+            Queue<int> player1,
+            Queue<int> player2)
+        {
+            while (player1.Any() && player2.Any())
+            {
+                var (card1, card2) = (player1.Dequeue(), player2.Dequeue());
+
+                if (card1 > card2)
+                {
+                    player1.Enqueue(card1);
+
+                    player1.Enqueue(card2);
+                }
+                else
+                {
+                    player2.Enqueue(card2);
+
+                    player2.Enqueue(card1);
+                }
+            }
+        }
+    }
+}
---
+++ src/AdventOfCode2020.Day22/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using AdventOfCode2020.Day22;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var (player1, player2) = (new Queue<int>(), new Queue<int>());
+
+var i = 0;
+
+while (!string.IsNullOrEmpty(lines[++i]))
+{
+    player1.Enqueue(int.Parse(lines[i]));
+}
+
+i++;
+
+while (++i < lines.Length)
+{
+    player2.Enqueue(int.Parse(lines[i]));
+}
+
+GameUtil.Play(player1, player2);
+
+var winner = player1.Any() ? player1 : player2;
+
+var solution1 = 0;
+
+checked
+{
+    for (i = winner.Count; i > 0; i--)
+    {
+        solution1 += i * winner.Dequeue();
+    }
+}
+
+Console.WriteLine($"Day 22 - Puzzle 1: {solution1}");
