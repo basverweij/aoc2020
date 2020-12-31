83ce08e056ac8a3e21b5f0b0192f1f7e948eb818
Bas Verweij <basverweij@users.noreply.github.com>
2020-12-31 09:00:20 +0100

    feat: complete day 21, puzzle 1

---
+++ src/AdventOfCode2020.Day21/FoodUtil.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2020.Day21
+{
+    public static class FoodUtil
+    {
+        public static (string[] ingredients, string[] allergens) Parse(
+            string s)
+        {
+            var ss = s[0..^1].Split(" (contains ");
+
+            var ingredients = ss[0].Split(' ');
+
+            var allergens = ss[1].Split(", ");
+
+            return (ingredients, allergens);
+        }
+    }
+}
---
+++ src/AdventOfCode2020.Day21/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using AdventOfCode2020.Day21;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var foods = lines
+    .Select(FoodUtil.Parse)
+    .ToArray();
+
+var options = new Dictionary<string, HashSet<string>>();
+
+foreach (var food in foods)
+{
+    foreach (var allergen in food.allergens)
+    {
+        if (options.TryGetValue(allergen, out var ingredients))
+        {
+            options[allergen].IntersectWith(food.ingredients);
+        }
+        else
+        {
+            options[allergen] = new(food.ingredients);
+        }
+    }
+}
+
+var possibleIngredients = options.Values.Aggregate(new HashSet<string>(), (a, b) => { a.UnionWith(b); return a; });
+
+var solution1 = foods.Sum(f => f.ingredients.Count(i => !possibleIngredients.Contains(i)));
+
+Console.WriteLine($"Day 21 - Puzzle 1: {solution1}");
