28643cf73d8e9f5ad09f036afab2896d74f1b38c
Bas Verweij <basverweij@users.noreply.github.com>
2021-01-01 17:36:04 +0100

    feat: complete day 25, puzzle 1

---
+++ src/AdventOfCode2020.Day25/EncryptionUtil.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode2020.Day25
+{
+    public static class EncryptionUtil
+    {
+        private const long _modulo = 20_201_227L;
+
+        public static (int cardLoopSize, int doorLoopSize) GetLoopSize(
+             long cardPublicKey,
+             long doorPublicKey,
+             long subjectNumber)
+        {
+            var (cardLoopSize, doorLoopSize) = (0, 0);
+
+            var key = 1L;
+
+            for (var i = 1; cardLoopSize == 0 || doorLoopSize == 0; i++)
+            {
+                key *= subjectNumber;
+
+                key %= _modulo;
+
+                if (key == cardPublicKey)
+                {
+                    cardLoopSize = i;
+                }
+
+                if (key == doorPublicKey)
+                {
+                    doorLoopSize = i;
+                }
+            }
+
+            return (cardLoopSize, doorLoopSize);
+        }
+
+        public static long GetEncryptionKey(
+            long publicKey,
+            int loopSize)
+        {
+            var key = 1L;
+
+            for (var i = 0; i < loopSize; i++)
+            {
+                key *= publicKey;
+
+                key %= _modulo;
+            }
+
+            return key;
+        }
+    }
+}
---
+++ src/AdventOfCode2020.Day25/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+using AdventOfCode2020.Day25;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var cardPublicKey = long.Parse(lines[0]);
+
+var doorPublicKey = long.Parse(lines[1]);
+
+var (cardLoopSize, doorLoopSize) = EncryptionUtil.GetLoopSize(cardPublicKey, doorPublicKey, 7);
+
+var solution1 = EncryptionUtil.GetEncryptionKey(doorPublicKey, cardLoopSize);
+
+Console.WriteLine($"Day 25 - Puzzle 1: {solution1}");
